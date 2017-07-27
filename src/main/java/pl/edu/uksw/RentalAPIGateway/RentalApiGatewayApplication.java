1109b60dded3677b3bc65409eead13523b7a145f
Nogazzi <p.noganski@gmail.com>
2017-07-27 12:14:37 +0200

    Initial commit

---
+++ src/main/java/pl/edu/uksw/RentalAPIGateway/RentalApiGatewayApplication.java
@@ -0,0 +1,12 @@
+package pl.edu.uksw.RentalAPIGateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentalApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentalApiGatewayApplication.class, args);
+	}
+}
---
+++ src/test/java/pl/edu/uksw/RentalAPIGateway/RentalApiGatewayApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.edu.uksw.RentalAPIGateway;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RentalApiGatewayApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
